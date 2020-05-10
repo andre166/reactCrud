92951a105734726f2a37cd70a97b4105e25855b7
andre <andremp1927@hotmail.com>
2020-05-10 20:00:32 -0300

    menu focus com states

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,45 +1,69 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../imagens/logo.png';
 import './header.css';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export default function Header() {
 
-    function toogleMenu(){
-        document.querySelector('#navbarNav').classList.remove('show');
+  let [menuFoco, setMenuFoco] = useState(["menu-1", "menu-2", "menu-3", "logo"]);
+  let [menuItem, setMenuItem] = useState(-1);
+
+  useEffect(() => {
+
+    if(menuItem == 3){
+        menuFoco.map((info) => (
+            removeAtivo(info)
+        ));
+        return;
     }
+
+    function foco(menuItem){
+        
+        menuFoco.map((info, index) => (
+            focar(info, index)
+        ));
+
+        function focar(info, index){
+            
+            if(menuItem == index){
+                let x = document.querySelector(`#${info}`);
+                x.classList.add('ativo');
+            }else{
+                let x = document.querySelector(`#${info}`);
+                x.classList.remove('ativo');
+            }    
+        }
+    }
+
+    foco(menuItem);
+
+    function removeAtivo(info){
+
+        let x = document.querySelector(`#${info}`);
+        x.classList.remove('ativo');
+    }
+
+  }, [menuItem]);
         
     return(
 
-        <nav class="navbar navbar-expand-lg sticky-top navbar-custom">
+        <Navbar collapseOnSelect expand="lg" className="navbar-custom sticky-top">
 
-            <Link to="/" id="logo-container">
-                    <img id="logo" src={logo} alt="Logo"/> 
-            </Link>
+            <Navbar.Brand onClick={()=> setMenuItem(3)}><Link to="/" id="logo-container"> <img id="logo" src={logo} alt="Logo"/> </Link></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span><i class="fas fa-bars"></i></span>
-            </button>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto"></Nav>
 
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto" id="menu" onClick={toogleMenu}>
-                    
-                    <li class="nav-item">
-                        <Link to="/ListaDeContatos" class="nav-link"><a ><i class="fas fa-list-alt"></i> Lista de Contatos</a></Link>
-                    </li>
+                <Nav id="menu">
+                    <Nav.Link as="li" id="menu-1" onClick={()=> setMenuItem(0)}><Link to="/ListaDeContatos" class="nav-link"><a ><i class="fas fa-list-alt"></i> Lista de Contatos</a></Link></Nav.Link>
+                    <Nav.Link as="li" id="menu-2" onClick={()=> setMenuItem(1)}><Link to="/CadastrarContato" class="nav-link"><a><i class="fas fa-user-plus"></i> Cadastrar Contatos</a></Link></Nav.Link>
+                    <Nav.Link as="li" id="menu-3" onClick={()=> setMenuItem(2)}><Link to="/Estatisticas" class="nav-link"><a><i class="fas fa-chart-pie"></i> Estatisticas</a></Link></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
 
-                    <li class="nav-item">
-                        <Link to="/CadastrarContato" class="nav-link"><a><i class="fas fa-user-plus"></i> Cadastrar Contatos</a></Link>
-                    </li>
-
-                    <li class="nav-item">
-                        <Link to="/Estatisticas" class="nav-link"><a><i class="fas fa-chart-pie"></i> Estatisticas</a></Link>
-                    </li>
-                
-                </ul>
-            </div>
-
-        </nav>
+        </Navbar>
 
     );
 }
